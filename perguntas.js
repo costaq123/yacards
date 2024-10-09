9a16541d2fbcd41903e6e751753a7118249ede58
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-10-09 11:42:10 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,104 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um círculo?',
+    'A = πr²')
+    
+    criaCartao(
+    'Matemática',
+    'O que é uma função injetora?',
+    'Uma função onde cada elemento do contradomínio possui no máximo uma imagem no domínio.')
+    
+    
+    criaCartao(
+    'Português',
+    'O que é um adjetivo?',
+    'Palavra que caracteriza um substantivo.')
+    
+    criaCartao(
+    'Português',
+    'Qual a diferença entre sujeito simples e composto?',
+    'Sujeito simples possui apenas um núcleo, enquanto o composto possui mais de um.')
+    
+    
+    criaCartao(
+    'História',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando')
+    
+    criaCartao(
+    'História',
+    'Quem foi o principal líder da Revolução Francesa?',
+    'Robespierre')
+    
+    criaCartao(
+    'Biologia',
+    'Qual a função do DNA?',
+    'Armazenar as informações genéticas')
+    
+    criaCartao(
+    'Biologia',
+    'O que é meiose?',
+    'Processo de divisão celular que reduz o número de cromossomos à metade.')
+    
+    criaCartao(
+    'Português',
+    'Qual o movimento literário que se caracterizou pela valorização da natureza e do índio como símbolo nacional?',
+    'Romantismo')
+    
+    criaCartao(
+    'Biologia',
+    'Qual a principal função da mitocôndria?', 'Produção de energia na forma de ATP.')
+    
+    criaCartao(
+    'Biologia',
+    'O que é um ecossistema?',
+    'Um sistema natural composto por todos os seres vivos e o ambiente físico em que vivem.')
+    
+    criaCartao(
+    'Biologia',
+    'O que é um alelo?',
+    'Uma das formas alternativas de um gene.')
+    
+    criaCartao(
+    'Português',
+    'O que é um texto dissertativo?',
+    'Um texto que tem como objetivo expor um ponto de vista sobre determinado assunto.')
+    
+    criaCartao(
+    'Português',
+    'Qual o movimento literário que valorizava a razão e a clareza?',
+    'Classicismo.')
+    
+    criaCartao(
+    'Matemática',
+    'O que é um polígono regular?',
+    'Um polígono que tem todos os lados e ângulos internos congruentes.')
+    
+    criaCartao(
+    'Biologia','Qual a principal função dos alvéolos pulmonares?',
+    'Realizar as trocas gasosas entre o ar inspirado e o sangue.')
+    
+    criaCartao(
+    'Biologia',
+    'O que é a inspiração?',
+    'O processo de entrada de ar nos pulmões.')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o principal fator que influencia o clima de uma região?',
+    'A latitude, pois ela determina a quantidade de insolação que a região recebe.')
+    
+    criaCartao(
+    'Geografia',
+    'O que é um bioma?',
+    'É um grande ecossistema terrestre, caracterizado por tipos específicos de vegetação e fauna, adaptados às condições climáticas e do solo.')
+    
+    criaCartao(
+    'Geografia',
+    'O que é a tectônica de placas?',
+    'É a teoria que explica a dinâmica da crosta terrestre, dividida em placas que se movimentam sobre o manto.')
+    
+    criaCartao(
+    'Geografia',
+    'O que é um bioma?',
+    'É um grande ecossistema terrestre, caracterizado por tipos específicos de vegetação e fauna, adaptados às condições climáticas e do solo.')
